e2d9472c54a2256ad8858370a57514c139ac33da
Sotarox <bluceislee@gmail.com>
2025-05-26 00:11:18 +0200

    setting local axios proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,9 @@ export default defineConfig({
     outDir: '../backend/src/main/resources/static',
     emptyOutDir: true
   },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+  },
 })
