1405389110afd9cb1b550f23f182091a76260eed
Sotarox <bluceislee@gmail.com>
2025-05-30 20:37:53 +0200

    optimize Sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,19 @@ import Sidebar from './components/Sidebar';
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
   const [selectedCountry, setSelectedCountry] = React.useState("");
+  const toggleIsSidebarOpen = React.useCallback(
+    () => setIsSidebarOpen(!isSidebarOpen), [isSidebarOpen]);
+  const setSelectedCountryWrapper = React.useCallback(
+    (countryIso2:string) => setSelectedCountry(countryIso2), []
+  )
 
   return (
     <>
-      <HeaderBar toggleDrawer={()=>setIsSidebarOpen(!isSidebarOpen)}/>
+      <HeaderBar toggleDrawer={toggleIsSidebarOpen}/>
         <Sidebar
           isOpen={isSidebarOpen} 
-          setIsOpen={() => setIsSidebarOpen(!isSidebarOpen)}
-          setSelectedCountry={(countryIso2) => setSelectedCountry(countryIso2)}
+          setIsOpen={toggleIsSidebarOpen}
+          setSelectedCountry={setSelectedCountryWrapper}
         />
         <Contents selectedCountry={selectedCountry}/>
       <BottomBar/>
