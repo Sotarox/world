cfd8a0912ee35be80696c71edcf2ee4f90da8dd7
Sotarox <bluceislee@gmail.com>
2025-05-30 15:25:22 +0200

    use ResponseEntity

--- backend/src/main/java/io/sotaro/backend/controller/AirportController.java
+++ backend/src/main/java/io/sotaro/backend/controller/AirportController.java
@@ -2,6 +2,7 @@ package io.sotaro.backend.controller;
 
 import io.sotaro.backend.model.AirportDto;
 import io.sotaro.backend.service.AirportService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class AirportController {
     }
 
     @GetMapping("/airports/{countryIso2}")
-    public List<AirportDto> getAirportsByCountryIso2(@PathVariable String countryIso2) {
-        return airportService.getAirportByCountryIso2(countryIso2);
+    public ResponseEntity<List<AirportDto>> getAirportsByCountryIso2(@PathVariable String countryIso2) {
+        return ResponseEntity.ok(airportService.getAirportByCountryIso2(countryIso2));
     }
 }
--- backend/src/main/java/io/sotaro/backend/controller/CountryController.java
+++ backend/src/main/java/io/sotaro/backend/controller/CountryController.java
@@ -2,6 +2,7 @@ package io.sotaro.backend.controller;
 
 import io.sotaro.backend.model.CountryDto;
 import io.sotaro.backend.service.CountryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,13 @@ public class CountryController {
     }
 
     @GetMapping("/countries")
-    public List<CountryDto> getAllCountries() {
-        return countryService.getAllCountries();
+    public ResponseEntity<List<CountryDto>> getAllCountries() {
+
+        return ResponseEntity.ok(countryService.getAllCountries());
     }
 
     @GetMapping("/countries/{countryIso2}")
-    public CountryDto getCountryByCountryIso2(@PathVariable String countryIso2) {
-        return countryService.getCountryByIso2(countryIso2);
+    public ResponseEntity<CountryDto> getCountryByCountryIso2(@PathVariable String countryIso2) {
+        return ResponseEntity.ok(countryService.getCountryByIso2(countryIso2));
     }
 }
