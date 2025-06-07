e0217ba77df36baa292827c216ad8d158354cca3
Sotarox <bluceislee@gmail.com>
2025-06-07 19:19:07 +0200

    format population number

--- frontend/src/pages/PopulationInfo.tsx
+++ frontend/src/pages/PopulationInfo.tsx
@@ -37,16 +37,17 @@ function PopulationInfo(props: PopulationInfoProps) {
         <Box sx={{ mb: 3 }}>
           <Grid container spacing={1}>
               <Grid size={{ xs:12 }}>
-                <InfoCard title="Population Information" value=""/>
+                <InfoCard title="Population ranking" value=""/>
               </Grid>
               <Grid size={{ xs: 6, md: 3 }}>
-                <InfoCard title="World rank:" 
-                  value={populationRankWorld ? populationRankWorld.rank.toString() : "N/A"}/>
+                <InfoCard title="World:" 
+                  value={populationRankWorld ? 
+                    formatRankInfo(populationRankWorld.rank, populationRankWorld.countCountries) : "N/A"}/>
               </Grid>
               <Grid size={{ xs: 6, md: 3 }}>
-                <InfoCard title="Continent rank:" 
+                <InfoCard title="Continent:" 
                   value={populationRankContinent ? 
-                    populationRankContinent.rank.toString() : "N/A"}/>
+                    formatRankInfo(populationRankContinent.rank, populationRankContinent.countCountries) : "N/A"}/>
               </Grid>
           </Grid>
         </Box>
@@ -56,4 +57,13 @@ function PopulationInfo(props: PopulationInfoProps) {
   }
 }
 
-export default PopulationInfo
+const formatRankInfo = (rank:number, countCountries:number): string => {
+  const order = 
+    rank === 1 ? "st" :
+    rank === 2 ? "nd" :
+    rank === 3 ? "rd" : 
+    "th";
+  return `${rank}${order} in ${countCountries} countries`;
+}
+
+export default PopulationInfo;
