570efc955b2d6d6051b3477710b1d9859511b074
Sotarox <bluceislee@gmail.com>
2025-06-11 12:34:42 +0200

    emojis to welcome page

--- frontend/src/pages/TopPage.tsx
+++ frontend/src/pages/TopPage.tsx
@@ -1,18 +1,21 @@
 import { Paper, Typography } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
 
 function TopPage() {
+  const theme = useTheme();
   return (
     <Paper sx={{ p: 2, m: 2}}>
-      <Typography variant='h2'>Welcome</Typography>
+      <Typography variant='h2' component='h1'>Welcome</Typography>
+      <Typography variant='body1' sx={{ mt: 2, whiteSpace: 'pre-line' }}>
+          🌍 This <span style={{fontWeight: 'bold', color: theme.palette.primary.light}}>World</span> is a simple web service that offers various data about countries.
+      </Typography>
       <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
         {`
-          “World” is a simple web service that offers various data about countries.
+          🐣 The current version is in alpha, which means only limited information is available.
 
-          The current version is in alpha, which means only limited information is available.
+          👓 The goal is to create a lightweight information system where users can quickly gather key data.
 
-          The goal is to create a lightweight information system where users can quickly gather key data.
-
-          UX theme: “Look & feel from shopping sites for informative content”.
+          💎 UX theme: “Look & feel from shopping sites for informative content”.
         `}
       </Typography>
     </Paper>
