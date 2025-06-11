37d682b77c0411c7144365276501ae9d249ec786
Sotarox <bluceislee@gmail.com>
2025-06-11 21:54:16 +0200

    Enrich About modal

--- frontend/src/components/AboutButton.tsx
+++ frontend/src/components/AboutButton.tsx
@@ -3,7 +3,8 @@ import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { Avatar, Divider } from '@mui/material';
+import { Avatar, Divider, Stack } from '@mui/material';
+import profileImageUrl from '../assets/sotaro_profile.jpg';
 
 const style = {
   position: 'absolute',
@@ -27,14 +28,16 @@ function BasicModal() {
     >
       <Paper sx={style}>
         <Typography variant='h4'>About</Typography>
-        <Divider sx={{mt:1, mb:1}}/>
+        <Divider sx={{mt:1, mb:2}}/>
         <Typography variant='h5'>Sotaro Shirai</Typography>
-        <Avatar>S</Avatar>
-        <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
-        {`Full-stack web developer, borned in Japan, lives in Stuttgart, Germany. Development is like playing a puzzle and Lego block at the same time.
-        `}
-        </Typography>
-        <Typography variant='h5' sx={{mt:1}}>Technologies</Typography>
+        <Stack direction="row" spacing={2} sx={{ml: 1, mt: 1.5, mb: 1}}>
+          <Avatar alt="Sotaro" src={profileImageUrl} sx={{ width: 72, height: 72 }}/>
+          <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
+          {`🥷 Full-stack web developer, borned in Japan 🇯🇵, lives in Stuttgart, Germany 🥨.`}
+          </Typography>
+        </Stack>
+        <br/>
+        <Typography variant='h5' sx={{mb: 1}}>Technologies in this web service</Typography>
         <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
           {`Spring Boot, React, TypeScript, Material UI, PostgresSQL, Docker, Amazon Lightsail, nginx, Ubuntu 
 
@@ -42,12 +45,12 @@ function BasicModal() {
         </Typography>
         <Typography variant='h5'>Upcoming Features</Typography>
         <ul>
-          <li>Map by OpenLayers or Leaflet</li>
-          <li>Customizable table by AG Grid</li>
-          <li>MongoDB integration (parallel to PostgresSQL)</li>
-          <li>CI/CD by Jenkins</li>
-          <li>End2End Test</li>
-          <li>More data from public APIs</li>
+          <li>🗺️ Map by OpenLayers or Leaflet</li>
+          <li>🔳 Customizable table by AG Grid</li>
+          <li>🤖 MongoDB integration (parallel to PostgresSQL)</li>
+          <li>👨‍🦳 CI/CD by Jenkins</li>
+          <li>👷‍♀️ End2End Test</li>
+          <li>✨ More data from public APIs</li>
         </ul>
       </Paper>
     </Modal>
