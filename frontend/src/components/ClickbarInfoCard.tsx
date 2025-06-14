d18f94198554297ac71646df9a4f9394a55f4b02
Sotarox <bluceislee@gmail.com>
2025-06-14 15:41:03 +0200

    Style cliclable country info

--- frontend/src/components/ClickbarInfoCard.tsx
+++ frontend/src/components/ClickbarInfoCard.tsx
@@ -1,7 +1,8 @@
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
-import { Button, Paper } from '@mui/material';
+import { Button, Paper, Stack } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { KeyboardArrowRight, KeyboardArrowDown } from '@mui/icons-material';
 
 const Item = styled(Paper)(({ theme }) => ({
               backgroundColor: '#fff',
@@ -23,16 +24,22 @@ function ClickbarInfoCard(props: ClickbarInfoCardProps) {
   const { isSelected, title, value, onClick } = props;
   const theme = useTheme();
   const borderProperties = isSelected ? `solid 10px ${theme.palette.secondary.main}`: '0px';
+  const Icon = isSelected ? <KeyboardArrowDown /> : <KeyboardArrowRight />;
   return (
     <Button onClick={() => onClick && onClick()} 
       sx={{alignItems:'start', padding: 1, flexGrow: 1, textAlign: 'left', textTransform: 'none'}}>
       <Item sx={{ borderLeft: borderProperties }}>
-        <Typography variant="h6" style={{ fontWeight: 50 }}>
-          {title}
-        </Typography>
-        <Typography variant="body1" style={{ fontWeight: 500 }}>
-          {value}
-        </Typography>
+        <Stack direction='row' sx={{alignItems: 'flex-start'}}>
+          {Icon}
+          <Stack direction='column'>
+          <Typography variant="h6" style={{ fontWeight: 50 }}>
+            {title}
+          </Typography>
+          <Typography variant="body1" style={{ fontWeight: 500 }}>
+            {value}
+          </Typography>
+          </Stack>
+        </Stack>
       </Item>
     </Button>
   )
