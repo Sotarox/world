4121c0a33503fd771a4b166a3189160b1da1edbe
Sotaro <38978403+Sotarox@users.noreply.github.com>
2025-07-09 19:22:58 +0200

    Feature/add new jenkins (#15)
    * add new Jenkinsfile
    
    * add CountryRepositoryTest

---
+++ Jenkinsfile
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+    environment {
+        NODEJS_HOME = "${tool 'jenkins-nodejs'}"
+        MAVEN_HOME = "${tool 'jenkins-maven'}"
+        PATH = "$NODEJS_HOME/bin:$MAVEN_HOME/bin:${env.PATH}"
+    }
+    stages {
+        stage ('Frontend Build') {
+            steps {
+                  dir('frontend') {
+                    sh "npm install"
+                    sh "npm run build"
+                  }
+            }
+        }
+        stage('Backend Build') {
+            steps {
+                echo "Building"
+                dir('backend') {
+                  sh "mvn clean install -DskipTests"
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                dir('backend') {
+                    sh "mvn test"
+                }
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "Deploy"
+            }
+        }
+    }
+}
---
+++ backend/src/test/java/io/sotaro/backend/repository/CountryRepositoryTest.java
@@ -0,0 +1,52 @@
+package io.sotaro.backend.repository;
+
+import io.sotaro.backend.model.CountryEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+public class CountryRepositoryTest {
+    @Autowired
+    CountryRepository countryRepository;
+
+    @Test
+    void givenSingleCountryInDB_whenFindByCorrectId_thenReturnCorrectEntity(){
+        CountryEntity countryEntity = buildCountryEntity("GER");
+        countryRepository.saveAndFlush(countryEntity);
+        Optional<CountryEntity> optionalCountryEntity = countryRepository.findByCountryIso2(countryEntity.getCountryIso2());
+        assertTrue(optionalCountryEntity.isPresent());
+        CountryEntity foundCountry = optionalCountryEntity.get();
+        assertEquals(countryEntity.getCountryIso2(), foundCountry.getCountryIso2());
+    }
+
+    @Test
+    void givenSingleCountryInDB_whenFindByNonExistingId_thenReturnEmptyEntity(){
+        CountryEntity countryEntity = buildCountryEntity("GER");
+        countryRepository.saveAndFlush(countryEntity);
+        Optional<CountryEntity> optionalCountryEntity = countryRepository.findByCountryIso2("USA");
+        assertTrue(optionalCountryEntity.isEmpty());
+    }
+
+    CountryEntity buildCountryEntity(String iso2){
+        CountryEntity countryEntity = new CountryEntity();
+        countryEntity.setId("");
+        countryEntity.setCapital("");
+        countryEntity.setCurrencyCode("");
+        countryEntity.setFipsCode("");
+        countryEntity.setCountryIso2(iso2);
+        countryEntity.setCountryIso3("");
+        countryEntity.setContinent("");
+        countryEntity.setCountryId("");
+        countryEntity.setCountryName("");
+        countryEntity.setCurrencyName("");
+        countryEntity.setCountryIsoNumeric("");
+        countryEntity.setPhonePrefix("");
+        countryEntity.setPopulation(0);
+        return countryEntity;
+    }
+}
