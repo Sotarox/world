7f6eab70fe586b01326583088c3ce38491c8af55
Sotarox <bluceislee@gmail.com>
2025-07-10 16:35:54 +0200

    add CountryController test

---
+++ backend/src/test/java/io/sotaro/backend/controller/CountryControllerTest.java
@@ -0,0 +1,65 @@
+package io.sotaro.backend.controller;
+
+import io.sotaro.backend.configuration.CustomCorsConfiguration;
+import io.sotaro.backend.configuration.SecurityConfig;
+import io.sotaro.backend.model.CountryDto;
+import io.sotaro.backend.service.CountryService;
+import io.sotaro.backend.service.PopulationRankService;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CountryController.class)
+@Import({SecurityConfig.class, CustomCorsConfiguration.class})
+public class CountryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CountryService countryService;
+
+    @MockitoBean
+    private PopulationRankService populationRankService;
+
+    private static final String BASE_URI = "/api/countries";
+
+    @Nested
+    class GetCountryByCountryIso2 {
+
+        @Test
+        void givenAnEntity_findByCorrectIso2_returnCorrectDto() throws Exception {
+            CountryDto countryDto = buildCountryDto("DE");
+            when(countryService.getCountryByIso2("DE")).thenReturn(countryDto);
+            mockMvc.perform(get(BASE_URI + "/DE"))
+                    .andExpect(status().isOk());
+        }
+    }
+
+    CountryDto buildCountryDto(String iso2) {
+        return new CountryDto(
+                0L,
+                "id",
+                "capital",
+                "currencyCode",
+                "fipsCode",
+                iso2,
+                "countryIso3",
+                "continent",
+                "countryId",
+                "countryName",
+                "currencyName",
+                "countryIsoNumeric",
+                "phonePrefix",
+                0
+        );
+    }
+}
