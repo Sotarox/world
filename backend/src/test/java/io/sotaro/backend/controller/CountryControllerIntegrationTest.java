8a9b849cb8f3b6553c614e619eddf84ecc83b69e
Sotarox <bluceislee@gmail.com>
2025-07-26 20:40:15 +0200

    improve test

--- backend/src/test/java/io/sotaro/backend/controller/CountryControllerIntegrationTest.java
+++ backend/src/test/java/io/sotaro/backend/controller/CountryControllerIntegrationTest.java
@@ -1,12 +1,18 @@
 package io.sotaro.backend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.sotaro.backend.model.CountryDto;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,13 +24,25 @@ public class CountryControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private static final String BASE_URI = "/api/countries";
 
-    @Test
-    void given_when_thenReturnCorrectDto() throws Exception {
-        mockMvc.perform(get(BASE_URI + "/US"))
-                .andDo(print())
-                .andExpect(status().isOk());
+    @Nested
+    class GetCountryByCountryIso2 {
+        @Test
+        void whenFindByCorrectIso2_thenReturnCorrectDto() throws Exception {
+            ResultActions resultActions = mockMvc.perform(get(BASE_URI + "/US"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+            MvcResult result = resultActions.andReturn();
+            String contentAsString = result.getResponse().getContentAsString();
+            CountryDto resultDto = objectMapper.readValue(contentAsString, CountryDto.class);
+            assertEquals("US", resultDto.countryIso2());
+            assertEquals("United States", resultDto.countryName());
+            assertEquals("Washington", resultDto.capital());
+        }
     }
 
 }
