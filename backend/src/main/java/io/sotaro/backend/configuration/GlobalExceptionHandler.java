2ada7fd3140eaadbc4f1c92fde8b1c44a0537de6
Sotarox <bluceislee@gmail.com>
2025-07-27 13:08:23 +0200

    improve GlobalExceptionHandler

--- backend/src/main/java/io/sotaro/backend/configuration/GlobalExceptionHandler.java
+++ backend/src/main/java/io/sotaro/backend/configuration/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.sotaro.backend.configuration;
 
 import io.sotaro.backend.exception.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +20,27 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("error", "Resource Not Found");
-        body.put("message", ex.getMessage());
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(body);
+    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(HttpServletRequest req, ResourceNotFoundException ex) {
+        return buildResponseBody(req, ex, HttpStatus.NOT_FOUND,
+                "Resource Not Found");
     }
 
     // For path/query/request-body parameter validation
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({ MethodArgumentNotValidException.class, HandlerMethodValidationException.class, ConstraintViolationException.class })
-    public ResponseEntity<Map<String, Object>> handleValidationException(Exception ex){
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("error", "Invalid format is used in request parameter");
-        body.put("message", ex.getMessage());
+    public ResponseEntity<Map<String, Object>> handleValidationException(HttpServletRequest req, Exception ex){
+        return buildResponseBody(req, ex, HttpStatus.BAD_REQUEST,
+                "Invalid format is used in request parameter");
+    }
 
+    private ResponseEntity<Map<String, Object>> buildResponseBody(HttpServletRequest req, Exception ex, HttpStatus httpStatus, String errorMessage) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("Timestamp", LocalDateTime.now());
+        body.put("Endpoint", String.join(" ", req.getMethod(), req.getRequestURI()));
+        body.put("Error overview", errorMessage);
+        body.put("Exception message", ex.getMessage());
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
+                .status(httpStatus)
                 .body(body);
     }
 }
