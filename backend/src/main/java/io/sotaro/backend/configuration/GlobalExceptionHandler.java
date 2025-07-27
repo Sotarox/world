ca09c3fa95fa73b6eab52fcd9859c0135f433f88
Sotarox <bluceislee@gmail.com>
2025-07-27 11:09:57 +0200

    add validation

--- backend/src/main/java/io/sotaro/backend/configuration/GlobalExceptionHandler.java
+++ backend/src/main/java/io/sotaro/backend/configuration/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package io.sotaro.backend.configuration;
 
 import io.sotaro.backend.exception.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -24,6 +27,20 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(body);
+    }
+
+    // For path/query/request-body parameter validation
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ MethodArgumentNotValidException.class, HandlerMethodValidationException.class, ConstraintViolationException.class })
+    public ResponseEntity<Map<String, Object>> handleValidationException(Exception ex){
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("error", "Invalid format is used in request parameter");
+        body.put("message", ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(body);
     }
 }
