b5aaaf280764af68dfad97c4d40c5b232e525408
Sotarox <bluceislee@gmail.com>
2025-08-01 19:51:32 +0200

    style

--- frontend/src/components/SearchButton.tsx
+++ frontend/src/components/SearchButton.tsx
@@ -54,71 +54,66 @@ function SearchButton() {
         <SearchIcon />
       </IconButton>
       <Modal disableRestoreFocus={true} open={open} onClose={() => reset()}>
-        <StyledPaper
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-end',
-          }}
-        >
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='close search modal'
-            onClick={() => setOpen(false)}
-          >
-            <CloseIcon />
-          </IconButton>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box sx={{ flexBasis: '80%' }}>
-              <TextField
-                id='outlined-controlled'
-                autoFocus
-                label='Country Name'
-                value={query}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setQuery(event.target.value);
-                }}
-                slotProps={{
-                  input: {
-                    startAdornment: (
-                      <InputAdornment position='start'>
-                        <SearchIcon />
-                      </InputAdornment>
-                    ),
-                  },
-                }}
-                sx={{ width: '100%' }}
-              />
-              <Divider sx={{ mt: 1, mb: 2 }} />
-              {results.length > 0 ? (
-                results.map((obj) => (
-                  <ListItem key={obj.countryIso2} disablePadding>
-                    <ListItemButton
-                      onClick={() => {
-                        navigate(`/countries/${obj.countryIso2.toLowerCase()}`);
-                        reset();
-                      }}
-                    >
-                      <ListItemIcon>
-                        <span
-                          className={`fi fi-${obj.countryIso2.toLowerCase()}`}
-                          style={{
-                            height: '24px',
-                            width: '24px',
-                            flexShrink: '0',
-                          }}
-                        ></span>
-                      </ListItemIcon>
-                      <ListItemText primary={obj.countryName} />
-                    </ListItemButton>
-                  </ListItem>
-                ))
-              ) : (
-                <Typography variant='body1'>No results found</Typography>
-              )}
-            </Box>
+        <StyledPaper sx={{ display: 'flex', flexDirection: 'row' }}>
+          <Box sx={{ flexBasis: '10%' }} />
+          <Box sx={{ flexBasis: '80%', pt: 6 }}>
+            <TextField
+              id='outlined-controlled'
+              autoFocus
+              label='Country Name'
+              value={query}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setQuery(event.target.value);
+              }}
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position='start'>
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+              sx={{ width: '100%' }}
+            />
+            <Divider sx={{ mt: 1, mb: 2 }} />
+            {results.length > 0 ? (
+              results.map((obj) => (
+                <ListItem key={obj.countryIso2} disablePadding>
+                  <ListItemButton
+                    onClick={() => {
+                      navigate(`/countries/${obj.countryIso2.toLowerCase()}`);
+                      reset();
+                    }}
+                  >
+                    <ListItemIcon>
+                      <span
+                        className={`fi fi-${obj.countryIso2.toLowerCase()}`}
+                        style={{
+                          height: '24px',
+                          width: '24px',
+                          flexShrink: '0',
+                        }}
+                      ></span>
+                    </ListItemIcon>
+                    <ListItemText primary={obj.countryName} />
+                  </ListItemButton>
+                </ListItem>
+              ))
+            ) : (
+              <Typography variant='body1'>No results found</Typography>
+            )}
+          </Box>
+          <Box sx={{ flexBasis: '10%' }}>
+            <IconButton
+              size='large'
+              edge='end'
+              color='inherit'
+              aria-label='close search modal'
+              onClick={() => reset()}
+            >
+              <CloseIcon />
+            </IconButton>
           </Box>
         </StyledPaper>
       </Modal>
