83db2a02e8b9043fc2f1684ea352f3af0ed0ab88
Sotarox <bluceislee@gmail.com>
2025-08-14 16:47:40 +0200

    update CountryControllerIntegrationTest

--- backend/src/test/java/io/sotaro/backend/controller/CountryControllerIntegrationTest.java
+++ backend/src/test/java/io/sotaro/backend/controller/CountryControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Sql(scripts = "/test_populate_countries.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/test_populate_airports.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/clean_up.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class CountryControllerIntegrationTest {
     @Autowired
@@ -43,6 +45,7 @@ public class CountryControllerIntegrationTest {
             assertEquals("US", resultDto.countryIso2());
             assertEquals("United States", resultDto.countryName());
             assertEquals("Washington", resultDto.capital());
+            assertEquals(1472, resultDto.totalNumberOfAirports());
         }
 
         @Test
--- backend/src/test/resources/clean_up.sql
+++ backend/src/test/resources/clean_up.sql
@@ -1,1 +1,2 @@
 DELETE FROM Countries;
+DELETE FROM Airports;
