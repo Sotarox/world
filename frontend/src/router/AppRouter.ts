6d9e34b996cd494711cf595ab1d8cbf4f9570d50
Sotarox <bluceislee@gmail.com>
2025-12-30 21:28:28 +0100

    add inquiry form

--- frontend/src/router/AppRouter.ts
+++ frontend/src/router/AppRouter.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router';
 import AppLayout from './AppLayout';
 import TopPage from '../pages/TopPage';
 import CountryLoad from '../pages/CountryLoad';
+import Inquiry from '../pages/Inquiry';
 
 const AppRouter = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ const AppRouter = createBrowserRouter([
     children: [
       { index: true, Component: TopPage },
       { path: 'countries/:iso2', Component: CountryLoad },
+      { path: 'inquiry', Component: Inquiry },
       { path: '*', Component: TopPage },
     ],
   },
