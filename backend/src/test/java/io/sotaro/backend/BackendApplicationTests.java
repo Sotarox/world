e4dccfa72cf54b15a9eda85468c50545683fdd27
Sotaro <38978403+Sotarox@users.noreply.github.com>
2025-12-31 22:26:04 +0100

    Feature/add email (#33)
    * fix test setting
    
    * add abstractIntegrationTest class

--- backend/src/test/java/io/sotaro/backend/BackendApplicationTests.java
+++ backend/src/test/java/io/sotaro/backend/BackendApplicationTests.java
@@ -1,12 +1,13 @@
 package io.sotaro.backend;
 
+import io.sotaro.backend.config.TestMailConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class BackendApplicationTests {
+class BackendApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
