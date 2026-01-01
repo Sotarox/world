31e22e3694cf6c130593b57c58f3d3b89940487b
Sotarox <bluceislee@gmail.com>
2026-01-01 18:18:39 +0100

    remove unnecessary test class

--- backend/src/test/java/io/sotaro/backend/BackendApplicationTests.java
+++ backend/src/test/java/io/sotaro/backend/BackendApplicationTests.java
@@ -1,13 +1,12 @@
 package io.sotaro.backend;
 
-import io.sotaro.backend.config.TestMailConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class BackendApplicationTests extends AbstractIntegrationTest {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
