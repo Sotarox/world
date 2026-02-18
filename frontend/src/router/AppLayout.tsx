ce41f372ba3f1f1df0da6c30cc2b070fb7fcb33a
Sotarox <bluceislee@gmail.com>
2026-02-18 17:19:59 +0100

    Tune sidebar behavior at mobile

--- frontend/src/router/AppLayout.tsx
+++ frontend/src/router/AppLayout.tsx
@@ -1,35 +1,31 @@
 import * as React from 'react';
 import HeaderBar from '../units/HeaderBar';
-// import Sidebar from '../units/Sidebar';
 import FloatingRandomButton from '../units/FloatingRandomButton';
 import { Outlet } from 'react-router';
 import { Toaster } from '@/components/ui/sonner';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarProvider } from '@/components/custom/sidebar';
 import { AppSidebar } from '@/units/AppSidebar';
 
 function AppLayout() {
-  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
-  const toggleIsSidebarOpen = React.useCallback(
-    () => setIsSidebarOpen(!isSidebarOpen),
-    [isSidebarOpen]
-  );
   return (
     // when mode is dark, tailwind applies corresponding styles to underlying elements
-    <SidebarProvider>
-      <div
-        className={`bg-neutral-100 dark:bg-gt-background font-display min-h-screen flex flex-col`}
-      >
-        <HeaderBar toggleDrawer={toggleIsSidebarOpen} />
-        {/* <Sidebar isOpen={isSidebarOpen} setIsOpen={toggleIsSidebarOpen} /> */}
-        <div className='flex p-2'>
-          <AppSidebar />
-          <SidebarTrigger className='size-10' />
-          <Outlet />
+    <div
+      className={`bg-neutral-100 dark:bg-gt-background font-display min-h-screen`}
+    >
+      <SidebarProvider>
+        <div className='flex flex-col w-full'>
+          <HeaderBar />
+          <div className='flex p-4 w-full'>
+            <AppSidebar />
+            <div className='flex-1'>
+              <Outlet />
+            </div>
+          </div>
+          <FloatingRandomButton />
+          <Toaster />
         </div>
-        <FloatingRandomButton />
-        <Toaster />
-      </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </div>
   );
 }
 
--- frontend/src/units/HeaderBar.tsx
+++ frontend/src/units/HeaderBar.tsx
@@ -1,43 +1,52 @@
 import React from 'react';
-import { PanelLeftIcon } from 'lucide-react';
 import ThemeSwitch from './ThemeSwitch';
 import SearchButton from './SearchButton';
 import HeaderLogo from './HeaderLogo';
 import BurgerMenu from './BurgerMenu';
 import { useTheme } from '@mui/material/styles';
-import { Button } from '../components/custom/button';
 import { cn } from '@/lib/utils';
+import { SidebarTrigger } from '@/components/custom/sidebar';
+import { Button } from '@/components/ui/button';
+import { useSidebar } from '@/components/custom/sidebar';
+import { PanelLeftIcon } from 'lucide-react';
 
-interface HeaderBarProps {
-  toggleDrawer: () => void;
-}
+const DummySidebarTrigger = () => {
+  return (
+    <Button variant='ghost' size='icon' className='size-10'>
+      <PanelLeftIcon />
+      <span className='sr-only'>Toggle Sidebar</span>
+    </Button>
+  );
+};
 
-const HeaderBar = React.memo((props: HeaderBarProps) => {
-  const { toggleDrawer } = props;
+const HeaderBar = React.memo(() => {
+  const { isMobile, openMobile } = useSidebar();
 
   return (
-    <>
-      <div
-        className={cn(
-          'w-full bg-primary dark:bg-gt-header flex items-center shadow-md',
-          // On small screen Header is fixed at the bottom
-          'fixed bottom-0 h-15 top-auto px-3',
-          // On larger screen Header is sticky at the top
-          'sm:sticky sm:top-0 sm:min-h-16 sm:px-6'
-        )}
-        style={{ zIndex: useTheme().zIndex.drawer + 1 }}
-      >
-        <HeaderLogo />
-        <Button variant='ghost' size='icon' onClick={toggleDrawer}>
-          <PanelLeftIcon className='size-6' />
-        </Button>
-        <div className='flex-1' />
-        <SearchButton />
-        <div className='flex-1' />
-        <ThemeSwitch />
-        <BurgerMenu />
-      </div>
-    </>
+    <div
+      className={cn(
+        'w-full bg-primary dark:bg-gt-header flex items-center shadow-md',
+        // On small screen, Header is fixed at the bottom
+        'fixed bottom-0 h-15 top-auto px-3',
+        // On larger screen, Header is sticky at the top
+        'sm:sticky sm:top-0 sm:min-h-16 sm:px-6'
+      )}
+      style={{ zIndex: useTheme().zIndex.drawer + 1 }}
+    >
+      <HeaderLogo />
+      {/* When sidebar is open on mobile, click on Trigger makes flashingly opens sidebar again.
+       * As a workaround, dummy button is shown */}
+      {openMobile && isMobile ? (
+        <DummySidebarTrigger />
+      ) : (
+        <SidebarTrigger className='size-10' />
+      )}
+      <div className='flex-1' />
+      <SearchButton />
+      <div className='flex-1' />
+      <ThemeSwitch />
+      <BurgerMenu />
+    </div>
   );
 });
 HeaderBar.displayName = 'HeaderBar';
