659edeb44e1fc5989fefdd1d2d7304de73407b0c
Sotarox <bluceislee@gmail.com>
2026-02-24 17:55:04 +0100

    Get useContext back in layout

--- frontend/src/app/layout-helper.tsx
+++ frontend/src/app/layout-helper.tsx
@@ -7,26 +7,29 @@ import { Toaster } from '@/components/ui/sonner';
 import { SidebarProvider } from '@/components/custom/sidebar';
 import { AppSidebar } from '@/units/AppSidebar';
 import { ThemeProvider } from '@/theme/theme-provider';
+import { CurrentTopicContextProvider } from '@/contexts/CurrentTopicContext';
 
 function LayoutHelper({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      {/* when mode is dark, tailwind applies corresponding styles to underlying elements */}
-      <div
-        className={`bg-neutral-100 dark:bg-gt-background font-display min-h-screen`}
-      >
-        <SidebarProvider>
-          <div className='flex flex-col'>
-            <HeaderBar />
-            <div className='flex w-svw'>
-              <AppSidebar />
-              <div className='min-w-0 p-4 flex-1'>{children}</div>
+      <CurrentTopicContextProvider>
+        {/* when mode is dark, tailwind applies corresponding styles to underlying elements */}
+        <div
+          className={`bg-neutral-100 dark:bg-gt-background font-display min-h-screen`}
+        >
+          <SidebarProvider>
+            <div className='flex flex-col'>
+              <HeaderBar />
+              <div className='flex w-svw'>
+                <AppSidebar />
+                <div className='min-w-0 p-4 flex-1'>{children}</div>
+              </div>
+              <FloatingRandomButton />
+              <Toaster />
             </div>
-            <FloatingRandomButton />
-            <Toaster />
-          </div>
-        </SidebarProvider>
-      </div>
+          </SidebarProvider>
+        </div>
+      </CurrentTopicContextProvider>
     </ThemeProvider>
   );
 }
