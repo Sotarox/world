e27521776aec848ea26783540dd935739fa7aecb
Sotarox <bluceislee@gmail.com>
2026-02-28 21:33:59 +0100

    layout About page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -2,36 +2,30 @@
 
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import { Divider, Stack } from '@mui/material';
 import {
   Avatar,
   AvatarFallback,
   AvatarImage,
 } from '@/components/shadcn/avatar';
+import { Card } from '@/components/shadcn/card';
 
 function About() {
   return (
-    <>
+    <Card className='p-4'>
       <Typography variant='h4'>About</Typography>
-      <Divider sx={{ mt: 1, mb: 2 }} />
-      <Typography variant='h5'>Sotaro Shirai</Typography>
-      <Stack direction='column' spacing={2} sx={{ ml: 1, mt: 1.5, mb: 1 }}>
-        <Avatar className='size-18'>
+      <div className='flex flex-col gap-4 w-full sm:w-fit'>
+        <Typography variant='h5'>Sotaro Shirai</Typography>
+        <Avatar className='size-18 self-center'>
           <AvatarImage src='/sotaro_profile.jpg' alt='Sotaro' />
           <AvatarFallback>Sotaro</AvatarFallback>
         </Avatar>
-      </Stack>
+      </div>
       <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
         {`🥷 Web developer, borned in Japan 🇯🇵, lives in Stuttgart, Germany 🥨.`}
       </Typography>
-      <br />
-      <Typography variant='h5' sx={{ mb: 1 }}>
-        Technologies in this web service
-      </Typography>
+      <Typography variant='h5'>Technologies in this web service</Typography>
       <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
-        {`Spring Boot, React, Next.js, TypeScript, Webpack, Tailwind CSS, shadcn/ui, Material UI, PostgresSQL, Docker, Amazon Lightsail, nginx, Ubuntu 
-
-         `}
+        {`Spring Boot, React, Next.js, TypeScript, Webpack, Tailwind CSS, shadcn/ui, Material UI, PostgresSQL, Docker, Amazon Lightsail, nginx, Ubuntu `}
       </Typography>
       <Typography variant='h5'>Upcoming Features</Typography>
       <ul>
@@ -42,15 +36,14 @@ function About() {
             ... Now running in Next.js&apos;s SSG mode
           </span>
         </li>
-        <li>🎨 Migration from Material UI to Tailwind CSS + Shadcn/ui</li>
         <li>🗺️ Map by OpenLayers or Leaflet</li>
         <li>✨ More data from public APIs</li>
         <li>
           ✅ <s>End2End Test</s>{' '}
-          <span className='text-gray-500 dark:text-gray-300'> Done</span>
+          <span className='text-gray-500 dark:text-gray-300'> ... Done</span>
         </li>
       </ul>
-    </>
+    </Card>
   );
 }
 
