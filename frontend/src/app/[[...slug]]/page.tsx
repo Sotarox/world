bdae3e3d0d376f34bc1f45e5318721c38cd2cd31
Sotarox <bluceislee@gmail.com>
2026-02-28 20:34:17 +0100

    add logo in home page

--- frontend/src/app/[[...slug]]/page.tsx
+++ frontend/src/app/[[...slug]]/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link, Typography } from '@mui/material';
 import { Card } from '@/components/shadcn/card';
+import Image from 'next/image';
 
 export function generateStaticParams() {
   // Only generate the root path as a static param
@@ -15,20 +16,27 @@ function HomePage() {
       <Typography variant='h2' component='h1'>
         Welcome
       </Typography>
-      <Typography variant='body1' sx={{ mt: 2, whiteSpace: 'pre-line' }}>
-        🌍 This <span className='text-primary font-bold'>World</span> is a
-        simple web service that offers various data about countries.
-      </Typography>
-      <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
-        {`
-          🐣 The current version is in alpha, which means only limited information is available.
-
-          👓 The goal is to create a lightweight information system where users can quickly gather key data.
-
-          💎 UX theme: “Look & feel from shopping sites for informative content”.
-        `}
-      </Typography>
-      <br />
+      <div className='flex justify-center'>
+        <Image
+          src='/world-rounded.png'
+          alt='World rounded logo'
+          width={240}
+          height={240}
+        />
+      </div>
+      <div>
+        <Typography variant='body1' sx={{ mt: 2, whiteSpace: 'pre-line' }}>
+          🌍 This <span className='text-primary font-bold'>World</span> is a
+          simple web app that offers various data about countries.
+        </Typography>
+        <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
+          ⛳ The goal is to explore building information system by trying many
+          approaches.
+        </Typography>
+        <Typography variant='body1' sx={{ whiteSpace: 'pre-line' }}>
+          💎 Responsive design is supported for smartphone users.
+        </Typography>
+      </div>
       <Link
         href='https://github.com/Sotarox/world'
         target='_blank'
