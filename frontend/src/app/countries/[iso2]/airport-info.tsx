734f885e2309e9d0841bc01a9d2357de51785033
Sotarox <bluceislee@gmail.com>
2026-03-11 16:55:11 +0100

    move Airport import for test purpose

--- frontend/src/app/countries/[iso2]/airport-info.tsx
+++ frontend/src/app/countries/[iso2]/airport-info.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import InfoCard from '@/components/world/info-card';
-import { Airport } from '@/model/airport';
 import { formatCoordinate } from '@/utils/utils';
 import LocalAirportIcon from '@mui/icons-material/LocalAirport';
 import { Card } from '@/components/shadcn/card';
+import { type Airport } from '@/model/airport';
 
 interface AirportInfoProps {
   airport: Airport | null;
