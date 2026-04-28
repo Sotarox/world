bf0e88158f298cb13446a7b50e0f031b48d58a19
Sotarox <bluceislee@gmail.com>
2026-04-28 18:17:20 +0200

    animate info-wrapper to slide-in

--- frontend/src/app/countries/[iso2]/country-info-wrapper.tsx
+++ frontend/src/app/countries/[iso2]/country-info-wrapper.tsx
@@ -3,12 +3,30 @@
 import 'flag-icons/css/flag-icons.min.css';
 import { CountryInfo } from './country-info';
 import { CountryInfoTopicsLoad } from './country-info-topics-load';
+import { useState } from 'react';
+import { cn } from '@/lib/utils';
+
+const switchVisibility = (callback: () => void) => {
+  setTimeout(() => {
+    callback();
+  }, 400);
+};
 
 function CountryInfoWrapper({ iso2 }: { iso2: string }) {
+  const [visible, setVisible] = useState(false);
+  switchVisibility(() => setVisible(true));
+
   return (
-    <div className='pb-2 sm:pb-0 flex flex-col gap-3'>
-      <CountryInfo iso2={iso2} />
-      <CountryInfoTopicsLoad iso2={iso2} />
+    <div className='p-2 sm:pb-0 flex flex-col gap-3 relative'>
+      <div
+        className={cn(
+          'transition-all duration-800 ease-in-out absolute w-full',
+          visible ? 'left-0 opacity-100' : '-left-80 opacity-0'
+        )}
+      >
+        <CountryInfo iso2={iso2} />
+        <CountryInfoTopicsLoad iso2={iso2} />
+      </div>
     </div>
   );
 }
