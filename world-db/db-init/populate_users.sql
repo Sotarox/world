4a3eea0fc49aa481c8de47e616d58cc6c0f3c49b
Sotarox <bluceislee@gmail.com>
2026-06-12 21:44:40 +0200

    adjust login form to mail

---
+++ world-db/db-init/populate_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO Users (mail, username, password) VALUES
+('user@test.com', 'test-user', '$2a$10$XX8C/AuxtuMclX5XsbaVkO3w2e4ITcvVDq80Lqp.IXw80En.7dD5K');
+-- The password above is 'user' for testing purpose.
