bf1e103ea8ed375cb4b8e8c65f0cb93f2c9e7ee5
Sotarox <bluceislee@gmail.com>
2026-07-30 18:32:54 +0200

    use query in airport-list

--- frontend/src/app/layout-helper.tsx
+++ frontend/src/app/layout-helper.tsx
@@ -6,6 +6,7 @@ import { Toaster } from '@/components/shadcn/sonner';
 import { SidebarProvider } from '@/components/custom/sidebar';
 import { AppSidebar } from '@/components/world/app-sidebar';
 import { ThemeProvider } from '@/contexts/theme-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function LayoutHelper({ children }: { children: React.ReactNode }) {
   return (
@@ -17,24 +18,26 @@ function LayoutHelper({ children }: { children: React.ReactNode }) {
       <div
         className={`bg-neutral-100 dark:bg-gt-background font-display h-dvh overflow-y-auto overscroll-y-none`}
       >
-        <SidebarProvider>
-          <div className='flex flex-col'>
-            <HeaderBar />
-            <div className='flex w-svw sm:pt-16'>
-              <AppSidebar />
-              <div
-                className='min-w-0 p-4 flex-1 sm:pb-4'
-                style={{
-                  paddingBottom:
-                    'calc(var(--bottom-bar-height) + env(safe-area-inset-bottom, 0px) + 1rem)',
-                }}
-              >
-                {children}
+        <QueryClientProvider client={new QueryClient()}>
+          <SidebarProvider>
+            <div className='flex flex-col'>
+              <HeaderBar />
+              <div className='flex w-svw sm:pt-16'>
+                <AppSidebar />
+                <div
+                  className='min-w-0 p-4 flex-1 sm:pb-4'
+                  style={{
+                    paddingBottom:
+                      'calc(var(--bottom-bar-height) + env(safe-area-inset-bottom, 0px) + 1rem)',
+                  }}
+                >
+                  {children}
+                </div>
               </div>
+              <Toaster />
             </div>
-            <Toaster />
-          </div>
-        </SidebarProvider>
+          </SidebarProvider>
+        </QueryClientProvider>
       </div>
     </ThemeProvider>
   );
