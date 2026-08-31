45659c92bd543f73b57bca761c520dc8750da35c
Sotarox <bluceislee@gmail.com>
2026-08-31 09:55:29 +0200

    add handle

--- frontend/src/components/world/sortable-info-card.tsx
+++ frontend/src/components/world/sortable-info-card.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { useSortable } from '@dnd-kit/react/sortable';
+import { GripVerticalIcon } from 'lucide-react';
 
 interface SortableInfoCardProps {
   index: number;
@@ -11,11 +12,20 @@ interface SortableInfoCardProps {
 
 function SortableInfoCard(props: SortableInfoCardProps) {
   const { index, title, value, className } = props;
-  const { ref } = useSortable({ id: title, index });
+  const { ref, handleRef } = useSortable({ id: title, index });
   return (
-    <div ref={ref} className={cn('flex flex-col p-2', className)}>
-      <span className='text-lg font-extralight'>{title}</span>
-      <span className='text-base'>{value}</span>
+    <div
+      ref={ref}
+      className={cn('flex p-2 items-center justify-between', className)}
+    >
+      <div className='flex flex-col'>
+        <span className='text-lg font-extralight'>{title}</span>
+        <span className='text-base'>{value}</span>
+      </div>
+      <GripVerticalIcon
+        className='size-3 min-w-3 cursor-grab text-gray-400 dark:text-gray-500'
+        ref={handleRef}
+      />
     </div>
   );
 }
